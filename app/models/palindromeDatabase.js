f46a2a358165e09b7cedf8c31923496fba40ab5f
MarySalemme <marysalemme1@gmail.com>
2017-09-30 19:52:03 +0100

    Adding getPalindromes and storePalindrome functions to model

--- app/models/palindromeDatabase.js
+++ app/models/palindromeDatabase.js
@@ -2,8 +2,19 @@ var PalindromeDatabase = function() {
   this._palindromes = [];
 }
 
+PalindromeDatabase.prototype.getPalindromes = function() {
+  return this._palindromes;
+};
+
 PalindromeDatabase.prototype.isPalindrome = function(input) {
   return input === this._reverse(input)
+};
+
+PalindromeDatabase.prototype.storePalindrome = function(palindrome) {
+  if (this.isPalindrome(palindrome)) {
+    this._palindromes.push(palindrome)
+  };
+  return this._palindromes;
 };
 
 PalindromeDatabase.prototype._reverse = function(input) {
--- test/palindromeDatabase.js
+++ test/palindromeDatabase.js
@@ -6,9 +6,28 @@ describe('PalindromeDatabase', function() {
 
   palindromeDatabase = new PalindromeDatabase();
 
+  describe('#getPalindromes', function() {
+    it("returns the stored palindromes", function () {
+      palindromeDatabase.storePalindrome('ABBA');
+      palindromeDatabase.storePalindrome('mom');
+      expect(palindromeDatabase.getPalindromes()).to.equal(palindromeDatabase._palindromes);
+    });
+  });
+
   describe('#isPalindrome', function() {
     it('returns true if the input is a palindrome', function() {
       expect(palindromeDatabase.isPalindrome('ABBA')).to.equal(true);
+    });
+
+    it('returns false if the input is not a palindrome', function() {
+      expect(palindromeDatabase.isPalindrome('Voldemort')).to.equal(false);
+    });
+  });
+
+  describe('#storePalindrome', function() {
+    it('saves the palindrome in an array', function() {
+      palindromeDatabase.storePalindrome('eve')
+      expect(palindromeDatabase.getPalindromes()).to.include('eve')
     });
   });
 });
